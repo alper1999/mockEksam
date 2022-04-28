15d7796efebd1bdb0329f9f24f23a193c814f2cf
alper1999 <alper.adiyaman@hotmail.com>
2022-04-28 15:53:55 +0200

    s

--- client/addMovie.jsx
+++ client/addMovie.jsx
@@ -50,7 +50,7 @@ function FormTextarea({ label, value, setValue }) {
     );
 }
 
- function AddMovie() {
+ export function AddMovie() {
     const [title, setTitle] = useState("");
     const [year, setYear] = useState("");
     const [director, setDirector] = useState("");
